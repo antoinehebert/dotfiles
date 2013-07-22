9d0e74f1fec5f04d0e4d9d9ed2e99e8afb17416e
Antoine Hebert <hebert.antoine@gmail.com>
2013-07-22 15:09:28 -0400

    install deps had an error

--- install_deps.sh
+++ install_deps.sh
@@ -4,4 +4,7 @@
 # # z, oh how i love you
 cd ~/bin
 git clone https://github.com/rupa/z.git
-chmod +x ~/code/z/z.sh
+chmod +x ~/bin/z/z.sh
+
+# for the c alias (syntax highlighted cat)
+sudo easy_install Pygments
